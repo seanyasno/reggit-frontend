4b4edc586d41218e6ecd87d9bee1d060d0a37e43
Sean <sean.yasno@gmail.com>
2020-06-16 23:18:59 +0300

    Added navbar.

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,13 +1,15 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {requireAuthentication} from '../utils';
 import {HomePage, LoginPage} from './pages';
+import {NavBar} from './components';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <Router>
-        <div>
+        <div className={'App'}>
+            <NavBar/>
             <Switch>
                 <Route path={'/'} exact component={requireAuthentication(HomePage)}/>
                 <Route path={'/login'} component={LoginPage}/>
--- src/views/components/index.tsx
+++ src/views/components/index.tsx
@@ -1,5 +1,7 @@
 import LoginForm from './login-form/login-form';
+import NavBar from './nav-bar/nav-bar';
 
 export {
-    LoginForm
+    LoginForm,
+    NavBar
 }
