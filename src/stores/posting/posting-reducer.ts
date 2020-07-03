7ef6dea59abe0b93e87b78c8805ea17c8ffa1087
Sean <sean.yasno@gmail.com>
2020-07-04 00:47:37 +0300

    Removed posting stuff related to redux.

--- src/stores/posting/posting-reducer.ts
+++
@@ -1,8 +0,0 @@
-import PostingAction from './posting-action';
-import {BaseReducer} from '../../models';
-
-export default class PostingReducer extends BaseReducer {
-    [PostingAction.GET_POST_BY_ID_FINISHED](state: object, action: object) {
-        return state;
-    }
-}
