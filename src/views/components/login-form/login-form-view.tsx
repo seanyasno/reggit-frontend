eeb777cd433f7f7988f7fe0aec0481d6492abb15
Sean <sean.yasno@gmail.com>
2020-07-17 21:07:41 +0300

    Worked a little bit on UI for login page.

--- src/views/components/login-form/login-form-view.tsx
+++ src/views/components/login-form/login-form-view.tsx
@@ -1,39 +1,60 @@
-import {Typography, TextField, Button, makeStyles} from '@material-ui/core';
+import {Typography, TextField, Button, makeStyles, Card} from '@material-ui/core';
 import ILoginFormViewProps from './login-form-view-props';
+import {useCardStyle} from '../../../constants';
 import React from 'react';
 
 const useStyles = makeStyles({
+    root: {
+        height: '80vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        alignSelf: 'center',
+    },
     form: {
         display: 'flex',
         flexDirection: 'column',
-        width: '50%',
         margin: 'auto'
+    },
+    inputField: {
+        margin: '0.5em 0',
+    },
+    login: {
+        margin: '1em 0',
+        borderRadius: '0.6em'
     }
 });
 
 export default (props: ILoginFormViewProps) => {
     const classes = useStyles();
     const {onChange, onSubmit} = props;
+    const cardStyle = useCardStyle();
 
     return (
-        <div>
-            <form className={classes.form} onSubmit={onSubmit}>
-                <Typography variant={'h4'}>Login</Typography>
-                <TextField
-                    label={'username'}
-                    variant={'outlined'}
-                    name={'username'}
-                    onChange={onChange}
-                />
-                <TextField
-                    label={'password'}
-                    variant={'outlined'}
-                    name={'password'}
-                    type={'password'}
-                    onChange={onChange}
-                />
-                <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
-            </form>
+        <div className={classes.root}>
+            <Card className={cardStyle.style} elevation={3}>
+                <form className={classes.form} onSubmit={onSubmit}>
+                    <Typography className={classes.title} variant={'h4'}>Sign In</Typography>
+                    <TextField
+                        className={classes.inputField}
+                        label={'Username'}
+                        name={'username'}
+                        onChange={onChange}
+                    />
+                    <TextField
+                        className={classes.inputField}
+                        label={'Password'}
+                        name={'password'}
+                        type={'password'}
+                        onChange={onChange}
+                    />
+                    <Button className={classes.login} type={'submit'} variant={'contained'} color={'primary'}>Submit</Button>
+                </form>
+            </Card>
         </div>
     );
 }
