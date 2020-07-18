0691a4209bb8ea0cdd6633e3d03c05a4af1acd86
Sean <sean.yasno@gmail.com>
2020-07-18 14:42:25 +0300

    Worked on design for the login page.

--- src/views/components/login-form/login-form-view.tsx
+++ src/views/components/login-form/login-form-view.tsx
@@ -1,60 +1,101 @@
-import {Typography, TextField, Button, makeStyles, Card} from '@material-ui/core';
+import {Typography, Checkbox, TextField, Button, makeStyles} from '@material-ui/core';
 import ILoginFormViewProps from './login-form-view-props';
-import {useCardStyle} from '../../../constants';
 import React from 'react';
 
 const useStyles = makeStyles({
     root: {
-        height: '80vh',
+        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        width: '25%',
+    },
+    titleSection: {
+        textAlign: 'start',
+        alignSelf: 'start',
+        margin: '1.5em 0'
     },
     title: {
-        textAlign: 'center',
-        fontWeight: 'bold',
-        alignSelf: 'center',
+        fontWeight: 'lighter',
     },
     form: {
         display: 'flex',
         flexDirection: 'column',
-        margin: 'auto'
+        margin: 'auto',
+        width: '100%',
+    },
+    bottomSection: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        textAlign: 'center',
+        alignItems: 'center',
+    },
+    innerBottomSection: {
+        display: 'flex',
+        textAlign: 'start',
+        justifyContent: 'left',
+        alignItems: 'center',
     },
     inputField: {
         margin: '0.5em 0',
     },
     login: {
         margin: '1em 0',
-        borderRadius: '0.6em'
+        padding: '0.6em',
+        borderRadius: '0.6em',
+        textTransform: 'none',
+        fontSize: 'large',
+        fontWeight: 'lighter',
+        backgroundColor: '#6C63FF',
+    },
+    registerSection: {
+        display: 'flex',
+        justifyContent: 'start'
+    },
+    register: {
+        fontWeight: 'bold',
+        marginLeft: '0.4em'
     }
 });
 
 export default (props: ILoginFormViewProps) => {
     const classes = useStyles();
     const {onChange, onSubmit} = props;
-    const cardStyle = useCardStyle();
 
     return (
         <div className={classes.root}>
-            <Card className={cardStyle.style} elevation={3}>
-                <form className={classes.form} onSubmit={onSubmit}>
-                    <Typography className={classes.title} variant={'h4'}>Sign In</Typography>
-                    <TextField
-                        className={classes.inputField}
-                        label={'Username'}
-                        name={'username'}
-                        onChange={onChange}
-                    />
-                    <TextField
-                        className={classes.inputField}
-                        label={'Password'}
-                        name={'password'}
-                        type={'password'}
-                        onChange={onChange}
-                    />
-                    <Button className={classes.login} type={'submit'} variant={'contained'} color={'primary'}>Submit</Button>
-                </form>
-            </Card>
+            <form className={classes.form} onSubmit={onSubmit}>
+                <div className={classes.titleSection}>
+                    <Typography className={classes.title} variant='h3'>Hello,</Typography>
+                    <Typography className={classes.title} variant='h3'>Welcome back</Typography>
+                </div>
+                <TextField
+                    className={classes.inputField}
+                    label={'Username'}
+                    name={'username'}
+                    onChange={onChange}
+                />
+                <TextField
+                    className={classes.inputField}
+                    label={'Password'}
+                    name={'password'}
+                    type={'password'}
+                    onChange={onChange}
+                />
+                <div className={classes.bottomSection}>
+                    <div className={classes.innerBottomSection}>
+                        <Checkbox color={'primary'}/>
+                        <Typography>Remember me</Typography>
+                    </div>
+                    <Typography>Forgot password?</Typography>
+                </div>
+                <Button className={classes.login} type={'submit'} variant={'contained'}
+                        color={'primary'} >Login</Button>
+                <div className={classes.registerSection}>
+                    <Typography>Don't have an account?</Typography>
+                    <Typography className={classes.register}>Click here</Typography>
+                </div>
+            </form>
         </div>
     );
 }
--- src/views/pages/login-page/login-page.tsx
+++ src/views/pages/login-page/login-page.tsx
@@ -1,11 +1,26 @@
+import loginImg from '../../../pictures/login_img.svg';
 import ILoginPageProps from './login-page-props';
+import {makeStyles} from '@material-ui/core';
 import {useHistory} from 'react-router-dom';
 import {LoginForm} from '../../components';
 import React, {useEffect} from  'react';
 import {connect} from 'react-redux';
 
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        justifyContent: 'space-evenly'
+    },
+    loginImg: {
+        maxHeight: '100vh',
+        maxWidth: '50%',
+    }
+});
+
 const LoginPage = (props: ILoginPageProps) => {
     const history = useHistory();
+    const classes = useStyles(props);
+
     useEffect(() => {
         if (props.isAuthenticated) {
             history.push('/');
@@ -13,7 +28,8 @@ const LoginPage = (props: ILoginPageProps) => {
     });
 
     return(
-      <div>
+      <div className={classes.root}>
+          <img className={classes.loginImg} src={loginImg} alt={''}/>
           <LoginForm/>
       </div>
     );
