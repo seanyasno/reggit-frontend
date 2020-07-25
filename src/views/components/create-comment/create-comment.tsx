c973088f0c8a8c9a9167db1b4a84d84a2516d2dd
Sean <sean.yasno@gmail.com>
2020-07-25 22:18:16 +0300

    Refactored creating new comment.

--- src/views/components/create-comment/create-comment.tsx
+++ src/views/components/create-comment/create-comment.tsx
@@ -3,9 +3,7 @@ import ICreateCommentProps from './create-comment-props';
 import {AuthenticationContext} from '../../../contexts';
 import React, {useContext, useState} from 'react';
 import {useCardStyle} from '../../../constants';
-import Config from '../../../conf/Config';
-import axios from 'axios';
-import _ from 'lodash';
+import CommentingController from '../../../controllers/commenting-controller';
 
 const useStyles = makeStyles({
     root: {
@@ -27,18 +25,15 @@ const CreateComment: React.FunctionComponent<ICreateCommentProps> = (props) => {
     const [content, setContent] = useState<string>('');
 
     const createComment = async () => {
-        if (_.isEmpty(content)) {
-            alert("Can't create an empty comment.");
-            return;
-        }
+        if (!user) return;
 
-        const url = Config.getInstance().getServerUrl() + Config.getInstance().getConfiguration().ROUTES.COMMENT.CREATE + postId;
-        await axios.post(url, {
-            userId: user?.id,
-            content
-        });
-        setContent('');
-        window.location.reload();
+        try {
+            await CommentingController.createComment(postId, user.id, content);
+            setContent('');
+            window.location.reload();
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return (
